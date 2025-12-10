30d4290f049f0f318a16b723da7c3d1da9a5bad1
arufars <firselee@gmail.com>
2025-12-10 16:05:22 +0700

    remove file unused

--- app/learning/taxonomy/v2/layout.tsx
+++
@@ -1,5 +0,0 @@
-import type { ReactNode } from "react"
-
-export default function TaxonomyV2Layout({ children }: { children: ReactNode }) {
-  return <div className="min-h-screen bg-white">{children}</div>
-}
